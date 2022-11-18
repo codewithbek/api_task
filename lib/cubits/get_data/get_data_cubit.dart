7490c00a3e66d3c054e09fb3024130e4c4cbb412
Asadbek <106092435+AsadbekMobileDeveloper@users.noreply.github.com>
2022-11-18 15:38:48 +0500

    cubit is done

--- lib/cubits/get_data/get_data_cubit.dart
+++ lib/cubits/get_data/get_data_cubit.dart
@@ -1,8 +1,34 @@
+import 'package:api_task/models/nbu_model.dart';
+import 'package:api_task/repository/nbu_repository.dart';
 import 'package:bloc/bloc.dart';
+import 'package:dio/dio.dart';
+import 'package:equatable/equatable.dart';
+import 'package:formz/formz.dart';
 import 'package:meta/meta.dart';
 
 part 'get_data_state.dart';
 
 class GetDataCubit extends Cubit<GetDataState> {
-  GetDataCubit() : super(GetDataInitial());
+  GetDataCubit({required NbuRepository nbuRepository})
+      : _nbuRepository = nbuRepository,
+        super(const GetDataState(currencyData: []));
+
+  final NbuRepository _nbuRepository;
+  void getCategories() async {
+    emit(state.copyWith(status: FormzStatus.submissionInProgress));
+    try {
+      final currencyData = await _nbuRepository.getAllData();
+      emit(state.copyWith(
+        currencyData: currencyData,
+        status: FormzStatus.submissionSuccess,
+      ));
+    } on DioError catch (error) {
+      emit(
+        state.copyWith(
+          status: FormzStatus.submissionFailure,
+          errorText: error.toString(),
+        ),
+      );
+    }
+  }
 }
--- lib/cubits/get_data/get_data_state.dart
+++ lib/cubits/get_data/get_data_state.dart
@@ -1,6 +1,28 @@
 part of 'get_data_cubit.dart';
 
-@immutable
-abstract class GetDataState {}
 
-class GetDataInitial extends GetDataState {}
+
+class GetDataState extends Equatable {
+  const GetDataState({
+    required this.currencyData,
+    this.errorText = '',
+    this.status = FormzStatus.pure,
+  });
+  final List<NbuModel> currencyData;
+  final String errorText;
+  final FormzStatus status;
+
+  GetDataState copyWith({
+    List<NbuModel>? currencyData,
+    String? errorText,
+    FormzStatus? status,
+  }) =>
+      GetDataState(
+        currencyData: currencyData ?? this.currencyData,
+        errorText: errorText ?? this.errorText,
+        status: status ?? this.status,
+      );
+
+  @override
+  List<Object> get props => [currencyData, status, errorText];
+}
