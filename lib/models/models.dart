905417378e79201f626c62b52950011057f54e15
abdulbosit209 <abdulbositbotirov2000@gmail.com>
2022-11-18 15:11:29 +0500

    added something

---
+++ lib/models/models.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class Models extends StatelessWidget {
+  const Models({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
